107d49f6d32c7e427b8f8d029a3b639515ff865a
doansabrina <108717804+doansabrina@users.noreply.github.com>
2022-09-23 13:14:57 -0400

    Add files via upload
    Condensed Titles

---
+++ 3 Condense.sql
@@ -0,0 +1,147 @@
+/****** Condense Titles Containing ' / ' to '/'  ******/
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],' / ','/')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'/ ','/')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],' /','/')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],' & ','/')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'-',' ')
+
+
+
+
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'ACAD SRV','ACADEMIC SERVICES')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'DB','DATABASE')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'MGMT','MANAGEMENT')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'PARAPROFESSIONAL','PARAPRO')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'PROFESSIONL','PROFESSIONAL')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'PROFESSNL','PROFESSIONAL')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'PR P','PUBLIC RELATIONS P')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'RSCH','RESEARCH')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'SR.','SR')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'SRVC','SERVICE')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'SRVS','SERVICES')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'SUPPRT','SUPPORT')
+
+
+
+
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'ADMIN SPECIALIST/COORDINATOR','ADMINISTRATIVE SPECIALIST/COORDINATOR')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'AG/FORESTRY TECH/PARAPRO','AGRICULTURE/FORESTRY TECHNICAL/PARAPRO')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'ALLIED HEALTH TECH/PARAPRO','ALLIED HEALTH TECHNICAL/PARAPRO')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'ANIMAL CARE TECHNICAL PARAPRO','ANIMAL CARE TECHNICAL/PARAPRO')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'ASSO/ASST DIRECTOR DIV/DEPT AD','ASSOC/ASST DIRECTOR, DIVISION/DEPARTMENT AD')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'ASSO/ASST DEPT CHAIR/DIR/HD AC','ASSOC/ASST DEPT CHAIR/DIRECTOR/HEAD AC')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'BUSINESS OPER PROFESSIONAL','BUSINESS OPERATIONS PARAPRO/PR')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'CHIEF ACCTING OFF/CONTROLLER','CHIEF ACCOUNTING OFFICER/CONTROLLER')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'CONTINUING ED PROFESSIONAL','CONTINUING EDUCATION PROFESSIONAL')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'DIRECTOR COLL/DIV/SCH AC','DIRECTOR, COLLEGE/DIVISION/SCHOOL AC')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'DIRECTOR DIV/DPT AD','DIRECTOR, DIVISION/DEPARTMENT AD')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'DIRECTOR SUBDIV/UNIT AD','DIRECTOR, SUBDIVISION/UNIT AD')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'DEPT CHAIR/HEAD AC','DEPARTMENT CHAIR/HEAD AC')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'DEV/FUNDRAISING PROFESSIONAL','DEVELOPMENT/FUNDRAISING PROFESSIONAL')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'FACILITY OPER PROFESSIONAL','FACILITY OPERATIONS PARA/PRO')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'OFFICE MANAGER/CLERICAL SUPR','OFFICE MANAGER/CLERICAL SUPERVISOR')
+ 
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'MARINE TECH/PARAPRO','MARINE TECHNICAL/PARAPRO')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'PHYSICAL/OCCUP THERAPIST','PHYSICAL/OCCUPATIONAL THERAPIST')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'PHYSICAL/OCCUP THERAPY TECH','PHYSICAL/OCCUPATIONAL THERAPY TECHNICIAN')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'SAFETY/RISK MANAGEMENT TECH/PARAPRO','SAFETY/RISK MANAGEMENT TECHNICAL/PARAPRO')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'SERVICE/MAINT FOREMAN/LEAD','SERVICE/MAINTENANCE FOREMAN/LEAD')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'TELECOMM TECH/PROFESSIONAL','TELECOMMUNICATIONS TECHNICAL/PROFESSIONAL')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'TEMP SERVICE/MAINTEN RETIREE','TEMPORARY SERVICE/MAINTENANCE')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'VET/ANIMAL SCI PROFESSIONAL','VETERINARY/ANIMAL SCIENCE PROFESSIONAL')
+
+
+
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'COORDINATOR','COOR')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'COOR','COORDINATOR')
+
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'TEMPORARY','TEMP')
+
+UPDATE [Salary].[dbo].[Compiled]
+SET [Title] = REPLACE ([Title],'TEMP','TEMPORARY')
